ab033809bdf8416ccbb51d04cf4838e6b6f80c36
Alex <kirt48alx@gmail.com>
2023-05-13 15:43:42 -0500

    feat: Menu modification

--- src/app/modules/main/menu-sidebar/menu-sidebar.component.ts
+++ src/app/modules/main/menu-sidebar/menu-sidebar.component.ts
@@ -1,66 +1,82 @@
-import {AppState} from '@/store/state';
-import {UiState} from '@/store/ui/state';
-import {Component, HostBinding, OnInit} from '@angular/core';
-import {Store} from '@ngrx/store';
-import {AppService} from '@services/app.service';
-import {Observable} from 'rxjs';
+import { AppState } from '@/store/state';
+import { UiState } from '@/store/ui/state';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppService } from '@services/app.service';
+import { Observable } from 'rxjs';
 
 const BASE_CLASSES = 'main-sidebar elevation-4';
 @Component({
-    selector: 'app-menu-sidebar',
-    templateUrl: './menu-sidebar.component.html',
-    styleUrls: ['./menu-sidebar.component.scss']
+  selector: 'app-menu-sidebar',
+  templateUrl: './menu-sidebar.component.html',
+  styleUrls: ['./menu-sidebar.component.scss']
 })
 export class MenuSidebarComponent implements OnInit {
-    @HostBinding('class') classes: string = BASE_CLASSES;
-    public ui: Observable<UiState>;
-    public user;
-    public menu = MENU;
+  @HostBinding('class') classes: string = BASE_CLASSES;
+  public ui: Observable<UiState>;
+  public user;
+  public menu = MENU;
 
-    constructor(
-        public appService: AppService,
-        private store: Store<AppState>
-    ) {}
+  constructor(
+    public appService: AppService,
+    private store: Store<AppState>
+  ) { }
 
-    ngOnInit() {
-        this.ui = this.store.select('ui');
-        this.ui.subscribe((state: UiState) => {
-            this.classes = `${BASE_CLASSES} ${state.sidebarSkin}`;
-        });
-        this.user = this.appService.user;
-    }
+  ngOnInit() {
+    this.ui = this.store.select('ui');
+    this.ui.subscribe((state: UiState) => {
+      this.classes = `${BASE_CLASSES} ${state.sidebarSkin}`;
+    });
+    this.user = this.appService.user;
+  }
 }
 
 export const MENU = [
-    {
-        name: 'Dashboard',
-        iconClasses: 'fas fa-tachometer-alt',
-        path: ['/']
-    },
-    {
-        name: 'Blank',
-        iconClasses: 'fas fa-file',
-        path: ['/blank']
-    },
-    {
-        name: 'Main Menu',
-        iconClasses: 'fas fa-folder',        
-        children: [
-            {
-                name: 'Sub Menu',
-                iconClasses: 'far fa-address-book',
-                path: ['/sub-menu-1']
-            },
-            {
-                name: 'Blank',
-                iconClasses: 'fas fa-file',
-                path: ['/sub-menu-2']
-            }
-        ]
-    },
-    {
-        name: 'Item 3',
-        iconClasses: 'fas fa-tachometer-alt',
-        path: ['/item3']
-    },
+  {
+    name: 'Dashboard',
+    iconClasses: 'fas fa-tachometer-alt',
+    path: ['/']
+  },
+  {
+    name: 'Configuración',
+    iconClasses: 'fas fa-cog',
+    children: [
+      {
+        name: 'Roles',
+        iconClasses: 'far fa-circle',
+        path: ['/sub-menu-1']
+      },
+      {
+        name: 'Usuarios',
+        iconClasses: 'far fa-circle',
+        path: ['/sub-menu-2']
+      }
+    ]
+  },
+  {
+    name: 'Empleados',
+    iconClasses: 'far fas fa-user-alt',
+    path: ['/blank']
+  },
+
+  {
+    name: 'Clientes',
+    iconClasses: 'far fas fa-user-friends',
+    path: ['']
+  },
+  {
+    name: 'Calendario',
+    iconClasses: 'far fa-calendar-alt',
+    path: ['']
+  },
+  {
+    name: 'Paquetes',
+    iconClasses: 'far fa-image',
+    path: ['']
+  },
+  {
+    name: 'Pedidos',
+    iconClasses: 'fas fa-columns',
+    path: ['']
+  }
 ];
