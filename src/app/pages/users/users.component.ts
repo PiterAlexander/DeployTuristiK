1f1b1816280f630f38807c73c9d990d315f40f3a
PiterAlexander <piteralexandercastrillongarcia@gmail.com>
2023-07-18 09:22:50 -0500

    modal de usuario modificada

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -21,17 +21,9 @@ interface State {
 export class UsersComponent {
   public ui: Observable<UiState>
   public userList: Array<User>
-  public filteredUsersList: Array<User>
-  public rolesList: Array<Role>
   public loading: boolean;
-  public search: string
-  public total: number
 
 
-  private _state: State = {
-    page: 1,
-    pageSize: 5,
-  }
 
   constructor(private store: Store<AppState>) { }
 
@@ -43,54 +35,17 @@ export class UsersComponent {
     this.ui.subscribe((state: UiState) => {
       this.userList = state.allUsers.data
       this.loading = state.allUsers.loading
-      this.rolesList = state.allRoles.data
-      this.searchByUserName();
     });
   }
 
-  matches(userResolved: User, term: string, pipe: PipeTransform) {
-    return (
-      userResolved.email.toLowerCase().includes(term.toLowerCase())
-    );
-  }
-
-  openCreateUserModal(user?: User) {
+  openCreateUserModal() {
     this.store.dispatch(new OpenModalUser())
   }
 
   openUpdateUserModal(user: User) {
+    console.log(user)
     this.store.dispatch(new OpenModalUser(user))
   }
 
-  searchByUserName() {
-    if (this.search === undefined || this.search.length <= 0) {
-      this.filteredUsersList = this.userList;
-      this.total = this.userList.length;
-      this.filteredUsersList = this.filteredUsersList.slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
-    } else {
-      this.filteredUsersList = this.userList.filter(user => user.email.toLocaleLowerCase().includes(this.search.toLocaleLowerCase().trim()));
-      this.total = this.filteredUsersList.length;
-      this.filteredUsersList = this.filteredUsersList.slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
-    }
-  }
-
-  get page() {
-    return this._state.page;
-  }
-  get pageSize() {
-    return this._state.pageSize;
-  }
-
-  set page(page: number) {
-    this._set({ page });
-  }
-  set pageSize(pageSize: number) {
-    this._set({ pageSize });
-  }
-
-  private _set(patch: Partial<State>) {
-    Object.assign(this._state, patch);
-    this.searchByUserName();
-  }
 
 }
--- src/app/store/ui/effects.ts
+++ src/app/store/ui/effects.ts
@@ -443,7 +443,7 @@ export class PackageEffects {
         switchMap((role) => {
             return this.apiService.deleteRole(role.roleId).pipe(
                 mergeMap((roleResolved) => {
-                  this.messageService.add({ key: 'alert-message', severity: 'success', summary: 'Exito', detail: 'Rol eliminado exitosamente' });
+                    this.messageService.add({ key: 'alert-message', severity: 'success', summary: 'Exito', detail: 'Rol eliminado exitosamente' });
                     return [
                         new DeleteRoleSuccess(roleResolved),
                         new GetAllRoleRequest(),
@@ -682,10 +682,12 @@ export class PackageEffects {
     openModalUser$ = createEffect(() =>
         this.actions$.pipe(
             ofType(userActions.OPEN_MODAL_USER),
-            tap(() => {
-                this.modalRef = this.modalService.open(CreateUserFormComponent, {
-                    backdrop: false,
-                    size: 'lg'
+            tap((action) => {
+                this.dialogRef = this.dialogService.open(CreateUserFormComponent, {
+                    /* Opciones del modal */
+                    header: action['payload'] === undefined ? 'Registrar Usuario' : 'Editar Usuario',
+                    width: '45%',
+                    contentStyle: { overflowY: 'auto' },
                 })
             })
         ), { dispatch: false });
@@ -696,7 +698,8 @@ export class PackageEffects {
         switchMap((user) => {
             return this.apiService.createUser(user).pipe(
                 mergeMap((userResolved) => {
-                    this.modalRef.close();
+                    this.dialogRef.close();
+                    this.messageService.add({ key: 'alert-message', severity: 'success', summary: 'Exito', detail: 'Usuario creado exitosamente.' });
                     return [
                         new CreateUserSuccess(userResolved),
                         new GetUsersRequest()
@@ -713,7 +716,9 @@ export class PackageEffects {
         switchMap((user) => {
             return this.apiService.updateUser(user.userId, user).pipe(
                 mergeMap((userResolved) => {
-                    this.modalRef.close(); return [
+                    this.dialogRef.close(); 
+                    this.messageService.add({ key: 'alert-message', severity: 'success', summary: 'Exito', detail: 'Usuario editado exitosamente' });
+                    return [
                         new UpdateUserSuccess(userResolved),
                         new GetUsersRequest(),
                     ]
